9403a3d8a4dda99ba61a48ba23e39d81ac975e02
lakshanrl2711 <lb4070110@gmail.com>
2025-08-31 03:42:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/lakshanrl2711/LAKI-BOT/blob/main/image/WhatsApp%20Image%202025-08-31%20at%2003.39.35_2e246770.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 🌺𝙇αкι 𝘽σƚ🍀 Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
